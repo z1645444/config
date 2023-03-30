7bead5c7571ae2081ec558bf9743d27d1eae5899
z1645444 <z1645444@outlook.com>
2023-03-30 19:08:10 +0800

    Update: wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,42 @@
-local wezterm = require 'wezterm';
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
 
-return {
-  font_size = 14.0,
-  color_scheme = "Gruvbox Dark",
-  font = wezterm.font_with_fallback({
-    "Fira Code",
-    -- "Source Han Sans SC VF",
-  }),
+-- This table will hold the configuration.
+local config = {}
 
-  hide_tab_bar_if_only_one_tab = true,
-  warn_about_missing_glyphs=false,
-  color_scheme = 'Gruvbox dark, medium (base16)',
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- color scheme
+config.color_scheme = 'Gruvbox dark, medium (base16)'
+
+-- font
+config.font_size = 14.0
+config.font = wezterm.font_with_fallback({"Fira Code", "Pingfang"})
+
+-- sweet warning
+config.warn_about_missing_glyphs = false
+
+-- hide tab bar if only one tab
+config.hide_tab_bar_if_only_one_tab = true
+
+-- key bind
+config.keys = {
+    {
+        key = 'UpArrow',
+        mods = "CMD",
+        action = wezterm.action.ScrollByPage(-0.5)
+    },
+    {
+        key = "DownArrow",
+        mods = "CMD",
+        action = wezterm.action.ScrollByPage(0.5)
+    },
 }
+
+-- and finally, return the configuration to wezterm
+return config
+
