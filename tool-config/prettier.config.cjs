a4277f64b4ac8a01931937594a3cf3257e1e3f03
z1645444 <z1645444@outlook.com>
2025-11-26 16:20:29 +0800

    feat: diverse prettier config

---
+++ tool-config/prettier.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 160,
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto',
+  proseWrap: 'preserve',
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: false,
+};
---
+++ tool-config/prettier.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  semi: true,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  embeddedLanguageFormatting: 'auto',
+  proseWrap: 'preserve',
+  bracketSameLine: false,
+  quoteProps: 'as-needed',
+  singleAttributePerLine: true,
+  vueIndentScriptAndStyle: false,
+};
+
+export default config;
