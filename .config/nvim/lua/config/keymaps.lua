e8d8b7b1fe9ed1776e382dd0c42f251be4ef14db
z1645444 <z1645444@outlook.com>
2025-12-10 16:01:14 +0800

    feat: nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,11 @@ map("n", "<leader>Y", '"+y', { noremap = true, silent = true })
 map("v", "<leader>Y", '"+y', { noremap = true, silent = true })
 map("n", "<leader>P", '"+p', { noremap = true, silent = true })
 map("v", "<leader>P", '"+p', { noremap = true, silent = true })
+
+-- disabled move line
+vim.keymap.del("n", "<A-j>")
+vim.keymap.del("n", "<A-k>")
+vim.keymap.del("i", "<A-j>")
+vim.keymap.del("i", "<A-k>")
+vim.keymap.del("v", "<A-j>")
+vim.keymap.del("v", "<A-k>")
